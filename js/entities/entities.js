bb492de0dccf9b9116bda3345d7e3e49e08c78e3
Josh Beard <josh@jlbeard.com>
2018-10-23 13:56:28 -0400

    Added sprite walking around

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -9,12 +9,60 @@ game.PlayerEntity = me.Entity.extend({
     init:function (x, y, settings) {
         // call the constructor
         this._super(me.Entity, 'init', [x, y , settings]);
+
+        this.body.setMaxVelocity(3, 3);
+        this.body.setFriction(0.4, 0.4);
+        this.body.gravity.x = 0;
+        this.body.gravity.y = 0;
+
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH, 0.4);
+
+        this.alwaysUpdate = true;
+
+        const walkSpeed = 250;
+
+        this.renderable.addAnimation("walkDown", [0, 1], walkSpeed);
+        this.renderable.addAnimation("walkLeft", [2, 3], walkSpeed);
+        this.renderable.addAnimation("walkUp", [4, 5], walkSpeed);
+        this.renderable.addAnimation("walkRight", [6, 7], walkSpeed);
+
+        this.renderable.setCurrentAnimation("walkDown");
     },
 
     /**
      * update the entity
      */
     update : function (dt) {
+
+        if (me.input.isKeyPressed("left")) {
+            this.body.force.x = -this.body.maxVel.x;
+
+            if (!this.renderable.isCurrentAnimation("walkLeft")) {
+                this.renderable.setCurrentAnimation("walkLeft");
+            }
+        } else if (me.input.isKeyPressed("right")) {
+            this.body.force.x = this.body.maxVel.x;
+
+            if (!this.renderable.isCurrentAnimation("walkRight")) {
+                this.renderable.setCurrentAnimation("walkRight");
+            }
+        } else if (me.input.isKeyPressed("up")) {
+            this.body.force.y = -this.body.maxVel.y;
+
+            if (!this.renderable.isCurrentAnimation("walkUp")) {
+                this.renderable.setCurrentAnimation("walkUp");
+            }
+        } else if (me.input.isKeyPressed("down")) {
+            this.body.force.y = this.body.maxVel.y;
+
+            if (!this.renderable.isCurrentAnimation("walkDown")) {
+                this.renderable.setCurrentAnimation("walkDown");
+            }
+        } else {
+            this.body.force.x = 0;
+            this.body.force.y = 0;
+        }
+
 
         // apply physics to the body (this moves the entity)
         this.body.update(dt);
--- js/game.js
+++ js/game.js
@@ -12,7 +12,7 @@ var game = {
     // Run on page load.
     "onload" : function () {
         // Initialize the video.
-        if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto"})) {
+        if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto", scaleMethod: "flex-width"})) {
             alert("Your browser does not support HTML5 canvas.");
             return;
         }
@@ -32,6 +32,11 @@ var game = {
 
         // add our player entity in the entity pool
         me.pool.register("mainPlayer", game.PlayerEntity);
+
+        me.input.bindKey(me.input.KEY.LEFT, "left");
+        me.input.bindKey(me.input.KEY.RIGHT, "right");
+        me.input.bindKey(me.input.KEY.UP, "up");
+        me.input.bindKey(me.input.KEY.DOWN, "down");
 
         // Start the game.
         me.state.change(me.state.PLAY);
